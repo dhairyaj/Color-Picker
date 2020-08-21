13a7b6e97d9b1f7882db6de8b6cf8e11f2c2c729
dhairyaj <dhairya.jain.work@gmail.com>
2020-08-22 02:35:05 +0530

    Minor changes and added comments

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,20 @@
+// Detect click on the refresh button
 $('.change').click(function(){
+    
+    // To select each color pane
     $('.color').each(function(){
+
+        // Get a random hexadecimal string of length 6 for the color
         var colors = '#' + Math.random().toString(16).substr(2,6);
+
+        // Apply the color to the color pane and change the hexcode
         $(this).css('background-color', colors);
         $(this).children('.hexcode').text(colors);
     });
+    // To start the function as soon as the page opens
 }).trigger('click');
 
+// To copy the hexcode in the clipboard
 $('.color').click(function(){
     var input = $("<input>");
     var color = $(this).children('.hexcode').text();
@@ -13,5 +22,7 @@ $('.color').click(function(){
     input.val(color).select();
     document.execCommand('copy');
     input.remove();
+    
+    // To display the alert of hexcode copied
     $('.copyalert').fadeIn().delay(2000).fadeOut();
 });
